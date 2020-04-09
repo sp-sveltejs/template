15f451d658c043d32e671f2afc03d3b63cee62a4
kayodebristol <kayode.bristol@gmail.com>
2020-04-09 16:40:50 +0200

    Add comment for config.js

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
 export let config = {
+    //Url of the SharePoint Site Collection being targeted for data
+    //Dev: When using npm run dev, this should be your sp-rest-proxy url
+    //Build: When using npm run build, this should be the actual Sharepoint Site Collection url you want @pnp/sp to point to
     baseUrl: "http://localhost:8080"
 }
